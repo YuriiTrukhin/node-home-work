8dee4a7238174e1deb9972efee8f03468831e385
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2021-01-22 13:59:18 +0200

    home-work

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.join("./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const list = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(list);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getContactById(contactId) {
+  const contactsData = await listContacts();
+  const findContact = contactsData.find((contact) => contact.id === contactId);
+  return findContact;
+}
+
+async function removeContact(contactId) {
+  const contactsData = await listContacts();
+  const filteredContacts = contactsData.filter((contact) => contact.id !== contactId);
+  fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+  return filteredContacts;
+}
+
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: uuidv4(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  const contactsData = await listContacts();
+  const filter = contactsData.filter((contact) => contact.id === newContact.id);
+  if (filter.length === 0) {
+    contactsData.push(newContact);
+    fs.writeFile(contactsPath, JSON.stringify(contactsData));
+    return contactsData;
+  } else {
+    console.log("такой айди уже существует");
+    return;
+  }
+}
+module.exports = { listContacts, getContactById, removeContact, addContact };
